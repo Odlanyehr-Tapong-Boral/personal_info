48a88454dbab4e25aded56173980e1c5b273fca7
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-11-20 11:50:18 +0800

    print of file

--- personinfo_input.py
+++ personinfo_input.py
@@ -1,24 +1,24 @@
 #Create a program that ask user for personal information.
 # Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc.
 with open("./output.txt", "a") as file_info :
-                (input("please input name "))
-                (input("please input age "))
-                (input("please input birthday "))
-                (input("please input address "))
-                (input("please input number "))
+                file_info.write(input("please input name\n "))
+                file_info.write(input("please input age\n "))
+                file_info.write(input("please input birthday\n "))
+                file_info.write(input("please input address\n "))
+                file_info.write(input("please input number\n "))
     
                 print(file_info)
                 
 while True:
-    var1 = (input("input another?"))
+    var1 = (input("input another? "))
     if var1 == 'yes' or var1 == 'Yes' or var1 == 'Y' or var1 == 'y':
         try:
             with open("./output.txt", "a") as file_info :
-                (input("please input name "))
-                (input("please input age "))
-                (input("please input birthday "))
-                (input("please input address "))
-                (input("please input number "))
+                file_info.write(input("please input name\n "))
+                file_info.write(input("please input age\n "))
+                file_info.write(input("please input birthday\n "))
+                file_info.write(input("please input address\n "))
+                file_info.write(input("please input number\n "))
     
                 print(file_info)
                 break
