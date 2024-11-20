46bdb0c41c8ea0ea7a6e4aaef62bb8ae832accb0
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-11-20 10:46:00 +0800

    place into file

--- personinfo.py
+++ personinfo.py
@@ -1,15 +1,14 @@
 #Create a program that ask user for personal information.
 # Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc.
-var1 = (input("please input name "))
-var2 = (input("please input age "))
-var3 = (input("please input birthday "))
-var4 = (input("please input address "))
-var5 = (input("please input number "))
-print("name is ",var1)
-print("age is",var2)
-print("birthday is",var3)
-print("address is",var4)
-print("number is",var5)
+with open("./output.txt", "a") as file_info :
+    var1 = (input("please input name "))
+    var2 = (input("please input age "))
+    var3 = (input("please input birthday "))
+    var4 = (input("please input address "))
+    var5 = (input("please input number "))
+    
+    print(file_info)
+
 # Write the collected information in a txt file.
 # It's up to you on how you'd like to format the information in the file.
 # The program should ask user if want to input another person or exit.
