ea0e3da628a81c9178b63c787b25d59d56e07eaf
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-11-21 17:12:03 +0800

    find info promp

--- personalinfo_output.py
+++ personalinfo_output.py
@@ -2,3 +2,13 @@ with open("./output.txt", "r") as file_info:
     lines = file_info.readlines()
     for line in lines :
         print(line.strip())
+        
+while True:
+        find = (input("Do you want to search for any information?  : "))
+        if find == 'yes' or find == 'Yes' or find == 'Y' or find == 'y':
+            find_info = (input("Enter the info you want: "))
+            
+        else:
+            print()
+            break
+        
