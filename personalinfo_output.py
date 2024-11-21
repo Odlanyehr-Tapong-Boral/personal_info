f109ef294f9968bfd186c132e1ba298317994006
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-11-21 18:51:21 +0800

    info find

--- personalinfo_output.py
+++ personalinfo_output.py
@@ -8,6 +8,21 @@ while True:
         if find == 'yes' or find == 'Yes' or find == 'Y' or find == 'y':
             find_info = (input("Enter the info you want: "))
             
+            try:
+                with open("./output.txt", "r") as file_info:
+                    lines = file_info.readlines()
+                    
+                    print("\n--- Search Results ---")
+                    for line in lines:
+                       if find_info in line:
+                          print(line.strip())
+                          
+                       else:
+                        print("No results found for your search.")
+            except FileNotFoundError:
+                            print("The file 'output.txt' does not exist. Please collect information first.")
+
+            
         else:
             print()
             break
