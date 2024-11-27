0893113b85050372035c715d4a2e834aedef0412
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-11-27 16:25:07 +0800

    removed unecessary code

--- personalinfo_output.py
+++ personalinfo_output.py
@@ -1,10 +1,5 @@
-with open("./output.txt", "r") as file_info:
-    lines = file_info.readlines()
-    for line in lines :
-        print(line.strip())
-        
 while True:
-        find = (input("Do you want to search for any information?  : "))
+        find = (input("Do you want to search for any info?  : "))
         if find == 'yes' or find == 'Yes' or find == 'Y' or find == 'y':
             find_info = (input("Enter the info you want: "))
             
@@ -13,16 +8,16 @@ while True:
                     lines = file_info.readlines()
                     
                     print("\n--- Search Results ---")
+                    
                     for line in lines:
                        if find_info in line:
                           print(line.strip())
                           
                        else:
-                        print("No results found for your search.")
-            except FileNotFoundError:
-                            print("The file 'output.txt' does not exist. Please collect information first.")
-
-            
+                            print("No results found for your search.")
+            except:
+                   print("no info found")
+                   
         else:
             print()
             break
